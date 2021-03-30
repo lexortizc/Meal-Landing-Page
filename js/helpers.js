29c8a31ce38cca98642d67e7e0f450e146a03fc5
Lex Ortiz <lex.ortizc@gmail.com>
2021-03-30 16:37:05 -0600

    feature: :lipstick: Category and Meals UI Styles

--- js/helpers.js
+++ js/helpers.js
@@ -24,15 +24,16 @@ export const renderMeal = meal => {
 export const renderMeals = (data, categoryName) => {
   let stringHTML = `<h2>${categoryName} Meals</h2>`;
   data.forEach( meal => {
-    stringHTML += `<article id="${meal.idMeal}" class="meal-item">
-      <h3 class="meal-title">${meal.strMeal}</h3>
+    stringHTML += `<article id="${meal.idMeal}" class="category-item">
+      <h3 title="${meal.strMeal}">${meal.strMeal}</h3>
       <img src="${meal.strMealThumb}" alt="${meal.strMeal}" title="${meal.strMeal}">
+      <button class='btn meal-btn' value='${meal.strMeal}'>Show recipe</button>
     </article>`;
   });
-  DOM.meals.innerHTML = stringHTML;
-  document.querySelectorAll('.meal-title').forEach( element => {
+  DOM.categories.innerHTML = stringHTML;
+  document.querySelectorAll('.meal-btn').forEach( element => {
     element.addEventListener('click', e => {
-      const QUERY = e.target.innerText;
+      const QUERY = e.target.value;
       getData(URL_MEALDETAILS.replace("QUERY", QUERY))
         .then( data => {
           renderMeal(data.meals[0], QUERY);
@@ -46,14 +47,15 @@ export const renderCategories = data => {
   let stringHTML = '<h2>Categories</h2>';
   data.forEach( category => {
     stringHTML += `<article id="${category.idCategory}" class="category-item">
-      <h3 class="category-title">${category.strCategory}</h3>
+      <h3 title="${category.strCategory}">${category.strCategory}</h3>
       <img src="${category.strCategoryThumb}" alt="${category.strCategory}" title="${category.strCategory}">
+      <button class='btn category-btn' value='${category.strCategory}'>Show dishes</button>
     </article>`;
   });
   DOM.categories.innerHTML = stringHTML;
-  document.querySelectorAll('.category-title').forEach( element => {
+  document.querySelectorAll('.category-btn').forEach( element => {
     element.addEventListener('click', e => {
-      const QUERY = e.target.innerText;
+      const QUERY = e.target.value;
       getData(URL_MEALS.replace("QUERY", QUERY))
         .then( data => {
           renderMeals(data.meals, QUERY);
